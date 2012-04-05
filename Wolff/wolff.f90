1627b1e507b4b5158c6d3d74800b718f8671cedb
Michael McQuiston <michael.mcquiston@gmail.com>
2012-04-05 12:41:34 -0400

    changed probability

--- Wolff/wolff.f90
+++ Wolff/wolff.f90
@@ -2,42 +2,51 @@
 PROGRAM wolff
       IMPLICIT NONE
       INTEGER, PARAMETER:: length = 10, latsize = length * length
-      INTEGER, PARAMETER:: steps = 500000
-      REAL*8, PARAMETER:: tau = 100
-      REAL*8, PARAMETER:: beta = 1 / tau
-      INTEGER:: lattice(latsize) = 1
-      INTEGER:: i, mag
-      REAL*8:: magsum = 0
+      INTEGER, PARAMETER:: eq = 100000, steps = 100000, tsteps = 6
+      REAL*8, PARAMETER:: tsize = 0.5
+      INTEGER:: lattice(latsize), sum_lat(latsize)
+      INTEGER:: i,j, mag
+      REAL*8:: tau, beta, av_lat(latsize), magav, magsum
 
       OPEN (UNIT=20,FILE="magnetization.dat",ACTION="write",STATUS="replace")
 
       !Initialize the seed
       CALL init_random_seed()
 
-      !Initialize the lattice
-      CALL init_lattice(lattice)
+      DO j = 1, tsteps
+        !Initialize variables
+        tau = tsize * DFLOAT(j)
+        beta = 1.0d0 / tau
+        sum_lat = 0
+        magsum = 0
 
-      !Initialize magnetization measurements
-      mag = SUM(lattice)
-      magsum = magsum + ABS(mag)
-      WRITE(20,*) 1,mag
+        !Initialize the lattice
+        CALL init_lattice(lattice)
 
-      !Do the wolff algorithm and take measurements for the given number of
-      !steps
-      DO i = 1, steps
 
-        !Flip a cluster
-        CALL flip_cluster(lattice)
+        CALL equilibrate(lattice)
 
-        !Measure magnetization
-        mag = SUM(lattice)
-        magsum = magsum + ABS(mag)
-        WRITE(20,*) i + 1, mag
+        !Initialize magnetization measurements
+        sum_lat = sum_lat + lattice
 
+        !Do the wolff algorithm and take measurements for the given number of
+        !steps
+        DO i = 1, steps
+
+          !Flip a cluster
+          CALL flip_cluster(lattice)
+
+          !Measure magnetization
+          sum_lat = sum_lat + lattice
+!          WRITE(20,*) i, SUM(lattice)
+          END DO
+
+        av_lat = ABS(DFLOAT(sum_lat)) / (steps + 1)
+        print *, SUM(sum_lat), SUM(av_lat), SUM(av_lat) / (latsize)
+        WRITE(20,*) tau, SUM(av_lat) / (latsize)
         END DO
+        CLOSE(20)
 
-      CLOSE(20)
-      print *, magsum/DFLOAT(steps + 1)
       CONTAINS
 ! initialize the seed for random numbers based on the cpu clock
         SUBROUTINE init_random_seed()
@@ -63,9 +72,17 @@ PROGRAM wolff
             END DO
           END SUBROUTINE
 
+! equilibrate cluster
+        SUBROUTINE equilibrate(lat)
+          INTEGER:: i, lat(latsize)
+            DO i = 1, eq
+              CALL flip_cluster(lat)
+              END DO
+            END SUBROUTINE
+
 ! build a wolff cluster and flip the cluster
         SUBROUTINE flip_cluster(lat)
-          INTEGER:: lat(latsize)
+          INTEGER:: lat(latsize), temp_lat(latsize)
           INTEGER :: queue(latsize)
           REAL:: rand1
           INTEGER:: head, tail
@@ -78,7 +95,9 @@ PROGRAM wolff
 
           !Choose random site to start building cluster
           CALL RANDOM_NUMBER(rand1)
-          queue(head) = int(rand1*latsize) + 1
+          queue(head) = INT(rand1*latsize) + 1
+
+          temp_lat = lat
 
           !flip first site
           site = queue(head)
@@ -92,7 +111,7 @@ PROGRAM wolff
             !Check North neighbor
             neighbor = north(site)
             will_bond = bond(neighbor,lat)
-            IF (((lat(site).EQ.(lat(neighbor)*(-1)))).AND.will_bond) THEN
+            IF ((lat(site).EQ.(lat(neighbor)*(-1))).AND.will_bond) THEN
               lat(neighbor) = lat(neighbor) * (-1)
               queue(tail) = neighbor
               tail = tail + 1
@@ -101,7 +120,7 @@ PROGRAM wolff
             !Check east neighbor
             neighbor = east(queue(head))
             will_bond = bond(neighbor,lat)
-            IF (((lat(site).EQ.(lat(neighbor)*(-1)))).AND.will_bond) THEN
+            IF ((lat(site).EQ.(lat(neighbor)*(-1))).AND.will_bond) THEN
               lat(neighbor) = lat(neighbor) * (-1)
               queue(tail) = neighbor
               tail = tail + 1
@@ -110,7 +129,7 @@ PROGRAM wolff
             !Check south neighbor
             neighbor = south(queue(head))
             will_bond = bond(neighbor,lat)
-            IF (((lat(site).EQ.(lat(neighbor)*(-1)))).AND.will_bond) THEN
+            IF ((lat(site).EQ.(lat(neighbor)*(-1))).AND.will_bond) THEN
               lat(neighbor) = lat(neighbor) * (-1)
               queue(tail) = neighbor
               tail = tail + 1
@@ -119,7 +138,7 @@ PROGRAM wolff
             !Check west neighbor
             neighbor = west(queue(head))
             will_bond = bond(neighbor,lat)
-            IF (((lat(site).EQ.(lat(neighbor)*(-1)))).AND.will_bond) THEN
+            IF ((lat(site).EQ.(lat(neighbor)*(-1))).AND.will_bond) THEN
               lat(neighbor) = lat(neighbor) * (-1)
               queue(tail) = neighbor
               tail = tail + 1
@@ -128,7 +147,6 @@ PROGRAM wolff
             !advance head
             head = head + 1
             END DO
-
           END SUBROUTINE
 
         !Return the index of the north neighbor
@@ -164,21 +182,13 @@ PROGRAM wolff
           INTEGER:: neigh, lat(latsize)
           REAL*8 :: mc, rand1
           INTEGER:: n,s,e,w, ein, ef, echan
-          !calculate change in energy
-          n = lat(north(neigh))
-          s = lat(south(neigh))
-          w = lat(west(neigh))
-          e = lat(east(neigh))
 
-          echan = 2 * (lat(neigh)) * (n + s + e + w)
-
-          mc = 1 - EXP((beta) * DFLOAT(-echan))
-
+          mc = EXP( - (beta))
           !generate random number
           CALL RANDOM_NUMBER(rand1)
 
           !determine whether a bond is built
-          bond = (rand1.GT.mc)
+          bond = (rand1.LT.mc)
 !          print *, mc, rand1, bond
           END FUNCTION bond
 
